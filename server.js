6b9e177cb86405ef6feb909413014a2cfdd7a7b4
codeconcept <contact@codeconcept.fr>
2019-12-30 14:56:20 +0100

    Sending back a JWT token on successful login

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
@@ -26,7 +28,11 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   console.log("req.body", req.body);
-  res.status(200).send(req.body);
+  const token = jwt.sign(
+    { text: "coucou", email: req.body.email },
+    process.env.JWT_SECRET
+  );
+  res.status(200).json(token);
 });
 
 app.get("/students", (req, res) => {
