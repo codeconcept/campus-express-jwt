721823ede1a7f310a21ac5be69b53d9f752db449
codeconcept <contact@codeconcept.fr>
2019-12-30 14:30:34 +0100

    Login POST

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 const students = [
   {
     id: 1,
-    firstName: "Erwan"
+    firstName: "Quentin"
   },
   {
     id: 2,
     firstName: "Camille"
   }
 ];
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+app.get("/login", (req, res) => {
+  res.send("Login page");
+});
+
+app.post("/login", (req, res) => {
+  console.log("req.body", req.body);
+});
 
 app.get("/students", (req, res) => {
   res.json(students);
